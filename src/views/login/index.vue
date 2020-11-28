f03e73c7e6fa691d5605a5d9377945198f76942d
liuli <nox60@hotmail.com>
2020-11-29 00:36:48 +0800

    权限模块

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -93,10 +93,18 @@
           </span>
         </el-form-item>
       </el-tooltip>
-
-      <el-button v-if="forLogin == 1" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
-      <el-button v-if="forLogin == 0" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">激活用户</el-button>
-      <el-button v-if="forLogin == 0" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">取消返回</el-button>
+<!--      <el-form-item>-->
+<!--        <el-button v-if="forLogin == 1" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>-->
+<!--        <el-button v-if="forLogin == 0" :span="8" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">激活用户</el-button>-->
+<!--        <el-button v-if="forLogin == 0" :span="8" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">取消返回</el-button>-->
+<!--      </el-form-item>-->
+      <el-row>
+        <el-col :span="24">
+          <el-button v-if="forLogin == 1" :span="24" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
+          <el-button v-if="forLogin == 0" :span="8" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">激活用户</el-button>
+          <el-button v-if="forLogin == 0" :span="8" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleActiveUser">取消返回</el-button>
+        </el-col>
+      </el-row>
     </el-form>
 
     <el-dialog title="Or connect with" :visible.sync="showDialog">
