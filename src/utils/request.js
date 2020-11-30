608d8125e00f83b0dc5eb2216a6c626f5bd36fe8
LiuLi <nox60@hotmail.com>
2020-11-30 14:22:23 +0800

    Update files

--- src/utils/request.js
+++ src/utils/request.js
@@ -15,12 +15,15 @@ service.interceptors.request.use(
   config => {
     // do something before request is sent
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
-    }
+    // if (store.getters.token) {
+    //   // let each request carry token
+    //   // ['X-Token'] is a custom headers key
+    //   // please modify it according to the actual situation
+    //   config.headers['X-Token'] = getToken()
+    // }
+    const xtoken = getToken()
+    console.log('before x-token, ', xtoken)
+    config.headers['X-Token'] = xtoken
     return config
   },
   error => {
