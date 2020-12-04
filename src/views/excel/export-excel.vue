2a2ebd0c70854a611c5639777f1e6e5e47268e87
nox60 <nox60@hotmail.com>
2020-12-04 23:46:18 +0800

    实现用户激活功能

--- src/views/excel/export-excel.vue
+++ src/views/excel/export-excel.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="app-container">
 
-    <div>
-      <FilenameOption v-model="filename" />
-      <AutoWidthOption v-model="autoWidth" />
-      <BookTypeOption v-model="bookType" />
-      <el-button :loading="downloadLoading" style="margin:0 0 20px 20px;" type="primary" icon="el-icon-document" @click="handleDownload">
-        Export Excel
-      </el-button>
-      <a href="https://panjiachen.github.io/vue-element-admin-site/feature/component/excel.html" target="_blank" style="margin-left:15px;">
-        <el-tag type="info">Documentation</el-tag>
-      </a>
-    </div>
-
   </div>
 </template>
 
