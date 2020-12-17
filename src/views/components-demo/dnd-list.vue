d6db4354b218fc698bc3c98e190c11ada0c08474
nox60 <nox60@hotmail.com>
2020-12-17 23:04:34 +0800

    实现用户激活功能

--- src/views/components-demo/dnd-list.vue
+++ src/views/components-demo/dnd-list.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="components-container">
-    <aside>drag-list base on
-      <a href="https://github.com/SortableJS/Vue.Draggable" target="_blank">Vue.Draggable</a>
-    </aside>
-    <div class="editor-container">
-      <dnd-list :list1="list1" :list2="list2" list1-title="List" list2-title="Article pool" />
-    </div>
-  </div>
+
 </template>
 
 <script>
