58014c70797c224b1a695b302d71a39cd14d7f65
LiuLi <nox60@hotmail.com>
2020-12-18 11:49:44 +0800

    Update files

--- src/views/components-demo/drag-kanban.vue
+++ src/views/components-demo/drag-kanban.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="components-container board">
-    <Kanban :key="1" :list="list1" :group="group" class="kanban todo" header-text="Todo" />
-    <Kanban :key="2" :list="list2" :group="group" class="kanban working" header-text="Working" />
-    <Kanban :key="3" :list="list3" :group="group" class="kanban done" header-text="Done" />
-  </div>
+
 </template>
 <script>
 import Kanban from '@/components/Kanban'
