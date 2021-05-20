9bafed541b4e5c613afc7538aa32cff79b79feb5
Liu Li <nox60@hotmail.com>
2021-05-20 23:10:30 +0800

    Update Export2Excel.js

--- src/vendor/Export2Excel.js
+++ src/vendor/Export2Excel.js
@@ -364,33 +364,3 @@ function s2ab(s) {
   return buf;
 }
 
-export function export_table_to_excel(id) {
-  var theTable = document.getElementById(id);
-  var oo = generateArray(theTable);
-  var ranges = oo[1];
-
-  /* original data */
-  var data = oo[0];
-  var ws_name = "SheetJS";
-
-  var wb = new Workbook(),
-    ws = sheet_from_array_of_arrays(data);
-
-  /* add ranges to worksheet */
-  // ws['!cols'] = ['apple', 'banan'];
-  ws['!merges'] = ranges;
-
-  /* add worksheet to workbook */
-  wb.SheetNames.push(ws_name);
-  wb.Sheets[ws_name] = ws;
-
-  var wbout = XLSX.write(wb, {
-    bookType: 'xlsx',
-    bookSST: false,
-    type: 'binary'
-  });
-
-  saveAs(new Blob([s2ab(wbout)], {
-    type: "application/octet-stream"
-  }), "test.xlsx")
-}
